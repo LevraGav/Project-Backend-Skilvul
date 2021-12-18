464f613081d3985dba8390c3d18c71f2cc18b363
tsaniawarda2 <63621268+tsaniawarda2@users.noreply.github.com>
2021-12-18 16:14:50 +0700

    create migration

---
+++ migrations/20211218085141-create-user.js
@@ -0,0 +1,50 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      fullname: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING(40),
+      },
+      conf_password: {
+        allowNull: false,
+        type: Sequelize.STRING(40),
+      },
+      avatar: {
+        type: Sequelize.ENUM("img1", "img2", "img3"),
+      },
+      role: {
+        type: Sequelize.ENUM("admin", "member"),
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Users");
+  },
+};
