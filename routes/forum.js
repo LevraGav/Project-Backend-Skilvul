7e29b6efc975d16c9f5c9d8bc8f4c9e8098888d1
deviayulestari <devialestari04@gmail.com>
2021-12-22 00:35:03 +0700

    adding authentication & authorization

--- routes/forum.js
+++ routes/forum.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const ForumController = require("../controllers/forum.controller");
 const router = express.Router();
 
-router.post("/", ForumController.postNewForum);
-router.get("/", ForumController.getAllForums);
+const ForumController = require("../controllers/forum.controller");
+const authentication = require("../middleware/authentication");
+const authorization = require("../middleware/authorization");
+
+router.post("/", authentication, authorization, ForumController.postNewForum);
+router.get("/",  ForumController.getAllForums);
 router.get("/:id", ForumController.getForumbyId);
-router.put("/:id", ForumController.updateForumById);
-router.delete("/:id", ForumController.deleteForumById);
+router.put("/:id", authentication, authorization, ForumController.updateForumById);
+router.delete("/:id", authentication, authorization, ForumController.deleteForumById);
 
 module.exports = router;
