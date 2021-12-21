49ce99e142fc9abd0f3f79ad0f6484b52bcc42a6
deviayulestari <devialestari04@gmail.com>
2021-12-22 00:32:49 +0700

    adding authentication & authorization

--- routes/tag.js
+++ routes/tag.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const TagController = require("../controllers/tag.controller");
 const router = express.Router();
 
-router.post("/", TagController.postNewTag);
+const TagController = require("../controllers/tag.controller");
+const authentication = require("../middleware/authentication");
+const authorization = require("../middleware/authorization");
+
+router.post("/", authentication, authorization, TagController.postNewTag);
 router.get("/", TagController.getAllTags);
 router.get("/:id", TagController.getTagbyId);
-router.put("/:id", TagController.updateTagById);
-router.delete("/:id", TagController.deleteTagById);
+router.put("/:id",  authentication, authorization, TagController.updateTagById);
+router.delete("/:id",  authentication, authorization, TagController.deleteTagById);
 
 module.exports = router;
