afdac46f757d84cd69ddc462a2a73f5f3fd6b8af
deviayulestari <devialestari04@gmail.com>
2021-12-22 23:26:35 +0700

    create new endpoints in favorite issue

--- routes/favoriteissues.js
+++ routes/favoriteissues.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const FavoriteIssuesController = require("../controllers/favoriteissues.controller");
-// const authentication = require("../middleware/authentication");
+const authentication = require("../middleware/authentication");
 
 // Authentication
-// router.use(authentication);
+router.use(authentication);
 router.get("/", FavoriteIssuesController.getAllFavoriteIssues);
+router.get("/user/:id", FavoriteIssuesController.getFavoriteIssuesByUserId);
 router.post("/", FavoriteIssuesController.postNewFavoriteIssues);
+router.delete("/:id", FavoriteIssuesController.deleteFavoriteIssueById);
 
 module.exports = router;
